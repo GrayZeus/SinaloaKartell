a1d453030741c6b288942d868c1f8a41cb9a9ca7
StGermain1111 <92604241+StGermain1111@users.noreply.github.com>
2023-02-23 13:34:56 +0100

    RSA, IRSA, RSAKey

--- src/main/java/RSAKey.java
+++ src/main/java/RSAKey.java
@@ -1,7 +1,4 @@
 import java.math.BigInteger;
 
 public record RSAKey(BigInteger part01, BigInteger part02) {
-    public String toString() {
-        return "(" + part01 + "," + part02 + ")";
-    }
 }
