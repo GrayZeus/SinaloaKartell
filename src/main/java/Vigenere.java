019d4e75a3545ebfdc9c2bcc9fa708c45d031788
SussyBohne <ste.hagner.21@lehre.mosbach.dhbw.de>
2023-02-23 18:49:27 +0100

    updated Vigenere, engine, Iengine and added dictionary

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -1,51 +1,139 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Vigenere  {
-    private Engine engine;
-    public void crack(String cipher, int keyspace) {
-        String key;
-        String clear;
-        int[] keyAscii = new int[keyspace];
-        Arrays.fill(keyAscii, 65);
-        double n = (Math.pow(26.0, keyspace));
+    public static final Map<Character, Integer> characterToIntegerMap;
+    public static final Map<Integer, Character> integerToCharacterMap;
+    private static String key;
 
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int j = 0; j < keyspace; j++) {
-            stringBuilder.append((char) keyAscii[j]);
-        }
-
-        key = stringBuilder.toString();
-
-        for (int i = 0; i < n; i++) {
-            clear = engine.encrypt(key, cipher);
-            System.out.println(key + ": " + clear);
-            key = nextKey(key);
-        }
+    static {
+        characterToIntegerMap = new HashMap<>();
+        characterToIntegerMap.put('A', 0);
+        characterToIntegerMap.put('a', 0);
+        characterToIntegerMap.put('B', 1);
+        characterToIntegerMap.put('b', 1);
+        characterToIntegerMap.put('C', 2);
+        characterToIntegerMap.put('c', 2);
+        characterToIntegerMap.put('D', 3);
+        characterToIntegerMap.put('d', 3);
+        characterToIntegerMap.put('E', 4);
+        characterToIntegerMap.put('e', 4);
+        characterToIntegerMap.put('F', 5);
+        characterToIntegerMap.put('f', 5);
+        characterToIntegerMap.put('G', 6);
+        characterToIntegerMap.put('g', 6);
+        characterToIntegerMap.put('H', 7);
+        characterToIntegerMap.put('h', 7);
+        characterToIntegerMap.put('I', 8);
+        characterToIntegerMap.put('i', 8);
+        characterToIntegerMap.put('J', 9);
+        characterToIntegerMap.put('j', 9);
+        characterToIntegerMap.put('K', 10);
+        characterToIntegerMap.put('k', 10);
+        characterToIntegerMap.put('L', 11);
+        characterToIntegerMap.put('l', 11);
+        characterToIntegerMap.put('M', 12);
+        characterToIntegerMap.put('m', 12);
+        characterToIntegerMap.put('N', 13);
+        characterToIntegerMap.put('n', 13);
+        characterToIntegerMap.put('O', 14);
+        characterToIntegerMap.put('o', 14);
+        characterToIntegerMap.put('P', 15);
+        characterToIntegerMap.put('p', 15);
+        characterToIntegerMap.put('Q', 16);
+        characterToIntegerMap.put('q', 16);
+        characterToIntegerMap.put('R', 17);
+        characterToIntegerMap.put('r', 17);
+        characterToIntegerMap.put('S', 18);
+        characterToIntegerMap.put('s', 18);
+        characterToIntegerMap.put('T', 19);
+        characterToIntegerMap.put('t', 19);
+        characterToIntegerMap.put('U', 20);
+        characterToIntegerMap.put('u', 20);
+        characterToIntegerMap.put('V', 21);
+        characterToIntegerMap.put('v', 21);
+        characterToIntegerMap.put('W', 22);
+        characterToIntegerMap.put('w', 22);
+        characterToIntegerMap.put('X', 23);
+        characterToIntegerMap.put('x', 23);
+        characterToIntegerMap.put('Y', 24);
+        characterToIntegerMap.put('y', 24);
+        characterToIntegerMap.put('Z', 25);
+        characterToIntegerMap.put('z', 25);
     }
-    private String nextKey(String key) {
-        int keyspace = key.length();
-        StringBuilder stringBuilder = new StringBuilder(key);
 
-        if ((int) key.charAt(keyspace - 1) == 90) {
-            for (int i = 1; i < keyspace; i++) {
-                if ((int) key.charAt(keyspace - i) == 90) {
-                    stringBuilder.setCharAt(keyspace - i, 'A');
-                    int current = stringBuilder.charAt(keyspace - (i + 1));
-                    char next = (char) (current + 1);
-                    stringBuilder.setCharAt(keyspace - (i + 1), next);
-                }
+    static {
+        integerToCharacterMap = new HashMap<>();
+        integerToCharacterMap.put(0, 'A');
+        integerToCharacterMap.put(1, 'B');
+        integerToCharacterMap.put(2, 'C');
+        integerToCharacterMap.put(3, 'D');
+        integerToCharacterMap.put(4, 'E');
+        integerToCharacterMap.put(5, 'F');
+        integerToCharacterMap.put(6, 'G');
+        integerToCharacterMap.put(7, 'H');
+        integerToCharacterMap.put(8, 'I');
+        integerToCharacterMap.put(9, 'J');
+        integerToCharacterMap.put(10, 'K');
+        integerToCharacterMap.put(11, 'L');
+        integerToCharacterMap.put(12, 'M');
+        integerToCharacterMap.put(13, 'N');
+        integerToCharacterMap.put(14, 'O');
+        integerToCharacterMap.put(15, 'P');
+        integerToCharacterMap.put(16, 'Q');
+        integerToCharacterMap.put(17, 'R');
+        integerToCharacterMap.put(18, 'S');
+        integerToCharacterMap.put(19, 'T');
+        integerToCharacterMap.put(20, 'U');
+        integerToCharacterMap.put(21, 'V');
+        integerToCharacterMap.put(22, 'W');
+        integerToCharacterMap.put(23, 'X');
+        integerToCharacterMap.put(24, 'Y');
+        integerToCharacterMap.put(25, 'Z');
+    }
+
+    public static String execute(String plainMessage, boolean encrypt) {
+        plainMessage = plainMessage.replace(" ", "");
+        char[] characterArray = plainMessage.toCharArray();
+        int[] integerArray = convertTextToIntegerArray(plainMessage.replace(" ", ""));
+        int[] keyIntegerArray = convertTextToIntegerArray(key.replace(" ", ""));
+
+        int keyIndex = 0;
+        for (int i = 0; i < characterArray.length; i++) {
+            int temp;
+            if (encrypt) {
+                temp = (integerArray[i] + keyIntegerArray[keyIndex]) % 26;
+            } else {
+                temp = (integerArray[i] - keyIntegerArray[keyIndex]) % 26;
             }
-        } else {
-            int current = stringBuilder.charAt(keyspace - 1);
-            char next = (char) (current + 1);
-            stringBuilder.setCharAt(keyspace - 1, next);
+
+            if (temp < 0) {
+                temp += 26;
+            }
+
+            char replacementChar = integerToCharacterMap.get(temp);
+            characterArray[i] = replacementChar;
+
+            keyIndex++;
+            if (keyIndex >= key.length()) {
+                keyIndex = 0;
+            }
         }
-        key = stringBuilder.toString();
-        return key;
+
+        return String.valueOf(characterArray);
     }
 
-    public void setEngine(Engine engine) {
-        this.engine = engine;
+    public static int[] convertTextToIntegerArray(String plaintext) {
+        int[] result = new int[plaintext.length()];
+
+        for (int i = 0; i < plaintext.length(); i++) {
+            result[i] = characterToIntegerMap.get(plaintext.charAt(i));
+        }
+
+        return result;
+    }
+    public static void setKey(String key) {
+        Vigenere.key = key.replace(" ", "");
     }
 }
